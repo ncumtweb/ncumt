7027c73dd0c9ef2c6d9622fe14fc2377044c0a37
Ben901227 <ben901227@gmail.com>
2024-08-17 03:42:08 +0800

    feat: 報名查詢加上 email 驗證碼

--- resources/views/conference/register.blade.php
+++ resources/views/conference/register.blade.php
@@ -6,7 +6,7 @@
     <section id="contact" class="contact mb-5">
         <div class="container" data-aos="fade-up">
             <div class="col-lg-12 text-center">
-                <h1 class="mt-3">113 年度第二十五屆全國大專校院 登山運動研討會</h1>
+                <h1>113 年度第二十五屆全國大專校院 登山運動研討會</h1>
                 <h1 class="page-title">報名表</h1>
             </div>
             <div class="row mb-5 justify-content-center">
