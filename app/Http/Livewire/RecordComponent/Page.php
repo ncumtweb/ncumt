7120fba33d2106bfc7df81a55574a31f658f4f08
noahh <noah940407@gmail.com>
2024-09-03 21:18:33 +0800

    review

--- app/Http/Livewire/RecordComponent/Page.php
+++ app/Http/Livewire/RecordComponent/Page.php
@@ -8,26 +8,29 @@ use Livewire\WithPagination;
 
 class Page extends Component
 {
-use WithPagination;
+    use WithPagination;
 
-protected $paginationTheme = 'bootstrap';
-public $amountOfPages = 5; // 每頁顯示 5 筆記錄
+    protected $paginationTheme = 'bootstrap';
+    public $amountOfPages = 5; // 每頁顯示 5 筆記錄
 
-public function render()
-{
+    public function render()
+    {
 // 獲取分頁資料
-$records = Record::orderBy('start_date', 'desc')->paginate($this->amountOfPages);
+        $records = Record::orderBy('start_date', 'desc')->paginate($this->amountOfPages);
 
 // 定義 $category_array
-$category_array = ["中級山", "高山", "溯溪"];
+        $category_array = ["中級山", "高山", "溯溪"];
 
-return view('livewire.record-component.page', [
-'records' => $records,
-'category_array' => $category_array,
-]);
-}
+        return view('livewire.record-component.page', [
+            'records' => $records,
+            'category_array' => $category_array,
+        ]);
+    }
 
-// 觸發滾動到頂端
+    /**
+     * 觸發滾動到頂端
+     * @return void
+     */
     public function updatedPage()
     {
         $this->dispatchBrowserEvent('scroll-to-top');
